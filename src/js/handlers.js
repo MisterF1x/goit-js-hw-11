c8a2ecee1762cdb357911dbffe1661f3b7640222
X Mister <someinkognito@gmail.com>
2023-03-04 23:38:53 +0200

    fix

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -30,16 +30,16 @@ export const onSubmitSearchInput = async e => {
   try {
     const { hits, total, totalHits } = await imgApiService.fetchImages();
 
+    if (hits.length === 0) {
+      return errorMsg(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
     imgApiService.totalImages = totalHits;
     imgApiService.incrementPage();
 
     Loading.standard('Loading...');
     setTimeout(() => {
-      if (hits.length === 0) {
-        return errorMsg(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
       Notify.info(`Hooray! We found ${total} images.`);
 
       clear(refs.gallery);
